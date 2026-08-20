854a74e5b2eea893707d49e4ee1c923f413082d8
lacsar712 <liug07115@gmail.com>
2026-08-20 14:36:19 +0800

    Clear the feeder breaker after a successful delivery.

--- internal/circuit/circuit.go
+++ internal/circuit/circuit.go
@@ -96,6 +96,17 @@ func (b *Breaker) Allow() Decision {
 }
 
 func (b *Breaker) Success() {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	switch b.state {
+	case Closed:
+		b.failures = 0
+	case HalfOpen:
+		b.state = Closed
+		b.failures = 0
+		b.probesLeft = 0
+	case Open:
+	}
 }
 
 func (b *Breaker) Failure() {
